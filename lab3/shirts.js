9de7f87735b681e7a6555bc8397507f20d4d854b
Zhigalo04 <vladzigalo@gmail.com>
2024-11-18 10:42:38 +0300

    lab3

---
+++ lab3/shirts.js
@@ -0,0 +1,92 @@
+const shirts = [{
+    "name": "Beep Boop",
+    "description": "Когда-то могучий проводник охранял перекрёсток Форбс и Морвуд и покорно направлял отвлечённых студентов колледжа, когда было безопасно перейти улицу. Его голос был успокаивающим, сильным и ровным. Его имя было «Beep Boop».",
+    "price": "$19.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/beepboop-white-front.png",
+            "back": "shirt_images/beepboop-white-back.png"
+        },
+        "blue": {
+            "front": "shirt_images/beepboop-blue-front.png",
+            "back": "shirt_images/beepboop-blue-back.png"
+        },
+        "pink": {
+            "front": "shirt_images/beepboop-pink-front.png",
+            "back": "shirt_images/beepboop-pink-back.png"
+        },
+        "red": {
+            "front": "shirt_images/beepboop-red-front.png",
+            "back": "shirt_images/beepboop-red-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+},
+{
+    "name": "Car",
+    "description": "Когда вы переезжаете в кампус, одно из первых воспоминаний многих студентов — это подъезд к общежитию, выгрузка сумок и заезд. Как они добираются до кампуса? На машине, конечно!",
+    "price": "$10.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/car-white-front.png",
+            "back": "shirt_images/car-white-back.png"
+        },
+        "blue": {
+            "front": "shirt_images/car-blue-front.png",
+            "back": "shirt_images/car-blue-back.png"
+        },
+        "green": {
+            "front": "shirt_images/car-green-front.png",
+            "back": "shirt_images/car-green-back.png"
+        },
+        "yellow": {
+            "front": "shirt_images/car-yellow-front.png",
+            "back": "shirt_images/car-yellow-back.png"
+        },
+        "red": {
+            "front": "shirt_images/car-red-front.png",
+            "back": "shirt_images/car-red-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-w-front.png",
+        "back": "shirt_images/default-w-back.png"
+    }
+},
+{
+    "name": "Forever Plaid",
+    "price": "$13.99",
+    "description": "С гордостью носите клетчатую клетку как нашивку на переднем кармане рубашки. А на обратной стороне задайте важный вопрос, на который все студенты CMU должны знать ответ: есть плед?",
+    "colors": {
+        "white": {
+            "front": "shirt_images/plaid-white-front.png",
+            "back": "shirt_images/plaid-white-back.png"
+        },
+        "pink": {
+            "front": "shirt_images/plaid-pink-front.png",
+            "back": "shirt_images/plaid-pink-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-w-front.png",
+        "back": "shirt_images/default-w-back.png"
+    }
+},
+{
+    "name": "BSUIR",
+    "description": "Миссия БГУИР — подготовка инженеров и ученых, способных генерировать и реализовывать инновационные идеи, создавать конкурентоспособную наукоемкую продукцию в сферах информатики и электроники.",
+    "price": "$6.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/bsuir-white-front.png",
+            "back": "shirt_images/bsuir-white-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+}];
